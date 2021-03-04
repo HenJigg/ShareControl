331d7250108393ba4806322e5121d88390db4ccc
henji <zhouhaogg789@outlook.com>
2021-03-04 21:54:54 +0800

    初始化控制系统代码, 添加Syncfusion许可

---
+++ ShareControl/ShareControl/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ShareControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(" NDA3MzU5QDMxMzgyZTMzMmUzMEdlU3h0djdLWkowQlV4dTF6MVd3VU1HT2dtVC9GNkhsdnQzQ2FzckxWSFU9");
+            base.OnStartup(e);
+        }
+    }
+}

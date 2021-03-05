74a9ef137d32db2c51114ff85775cdcfeecbd185
henji <zhouhaogg789@outlook.com>
2021-03-05 14:28:00 +0800

    添加dockmanager

---
+++ ShareControl/ShareControl/Assets/SolutionContract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShareControl.Assets
+{
+    public abstract class SolutionContract
+    {
+        public abstract string SolutionName { get; set; }
+
+        public abstract void GenerateDefaultSolutionCatalog();
+    }
+}
